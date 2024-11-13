02250b3275984124c92ed6ca914e0c4c030c74e4
sasha <118757762+zai4ikk@users.noreply.github.com>
2024-11-13 21:26:16 +0300

    Update drugstore.sql

--- server/drugstore.sql
+++ server/drugstore.sql
@@ -1,82 +1,122 @@
-CREATE DATABASE drugstore;
+CREATE SCHEMA IF NOT EXISTS `pharmacy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `pharmacy` ;
 
-USE drugstore;
-
--- Хранимая процедура для хэширования пароля
-DELIMITER //
-CREATE PROCEDURE HashPassword (IN inputPassword VARCHAR(255), OUT hashedPassword VARCHAR(255))
-BEGIN
-    SET hashedPassword = SHA2(inputPassword, 256);
-END //
-DELIMITER ;
-
--- Таблица назначений лекарств
-CREATE TABLE IF NOT EXISTS `assignationmedicament` (
+-- -----------------------------------------------------
+-- Table `pharmacy`.`assignationmedicament`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`assignationmedicament` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Groupmedicament` VARCHAR(25) NULL DEFAULT NULL,
+  `Groupmedicament` VARCHAR(255) NULL DEFAULT NULL,
   `Description` TEXT NULL DEFAULT NULL,
-  `Image` VARCHAR(50) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  `Image` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Таблица клиентов
-CREATE TABLE IF NOT EXISTS `client` (
+
+-- -----------------------------------------------------
+-- Table `pharmacy`.`client`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`client` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(50) NULL DEFAULT NULL,
-  `Login` VARCHAR(50) NOT NULL UNIQUE,
-  `Password` VARCHAR(255) NOT NULL,
-  `Representative` VARCHAR(50) NULL DEFAULT NULL,
+  `Name` VARCHAR(255) NULL DEFAULT NULL,
+  `Login` VARCHAR(255) NULL DEFAULT NULL,
+  `Password` VARCHAR(255) NULL DEFAULT NULL,
+  `Representative` VARCHAR(255) NULL DEFAULT NULL,
   `Adress` TEXT NULL DEFAULT NULL,
-  `Phonenumber` VARCHAR(15) NOT NULL UNIQUE,
-  PRIMARY KEY (`id`)
-);
+  `Phonenumber` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Таблица болезней
-CREATE TABLE IF NOT EXISTS `diseases` (
+
+-- -----------------------------------------------------
+-- Table `pharmacy`.`diseases`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`diseases` (
   `Illness` VARCHAR(255) NULL DEFAULT NULL,
-  `IDAssignationmedicament` INT NULL DEFAULT NULL
-);
+  `IDAssignationmedicament` INT NULL DEFAULT NULL,
+  INDEX `IDAssignationmedicament` (`IDAssignationmedicament` ASC) VISIBLE,
+  CONSTRAINT `diseases_ibfk_1`
+    FOREIGN KEY (`IDAssignationmedicament`)
+    REFERENCES `pharmacy`.`assignationmedicament` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Таблица сотрудников
-CREATE TABLE IF NOT EXISTS `employee` (
+
+-- -----------------------------------------------------
+-- Table `pharmacy`.`employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`employee` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Fullname` VARCHAR(50) NULL DEFAULT NULL,
-  `Post` VARCHAR(50) NULL DEFAULT NULL,
-  `Login` VARCHAR(50) NOT NULL UNIQUE,
-  `Password` VARCHAR(255) NOT NULL,
+  `Fullname` VARCHAR(255) NULL DEFAULT NULL,
+  `Post` VARCHAR(255) NULL DEFAULT NULL,
+  `Login` VARCHAR(255) NULL DEFAULT NULL,
+  `Password` VARCHAR(255) NULL DEFAULT NULL,
   `Birthdate` DATE NULL DEFAULT NULL,
   `Dateofhiring` DATE NULL DEFAULT NULL,
-  `Phonenumber` VARCHAR(15) NOT NULL UNIQUE,
-  `Photo` VARCHAR(50) NULL DEFAULT NULL,
-  `Education` VARCHAR(50) NULL DEFAULT NULL,
+  `Phonenumber` VARCHAR(255) NULL DEFAULT NULL,
+  `Photo` VARCHAR(255) NULL DEFAULT NULL,
+  `Education` VARCHAR(255) NULL DEFAULT NULL,
   `Salary` DECIMAL(10,2) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Таблица медикаментов
-CREATE TABLE IF NOT EXISTS `medicament` (
+
+-- -----------------------------------------------------
+-- Table `pharmacy`.`provider`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`provider` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(50) NULL DEFAULT NULL,
+  `Name` VARCHAR(255) NULL DEFAULT NULL,
+  `Representative` VARCHAR(255) NULL DEFAULT NULL,
+  `Post` VARCHAR(255) NULL DEFAULT NULL,
+  `Phonenumber` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `pharmacy`.`medicament`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`medicament` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(255) NULL DEFAULT NULL,
   `IDAssignationmedicament` INT NULL DEFAULT NULL,
   `IDProvider` INT NULL DEFAULT NULL,
-  `Unitsofmeasurement` VARCHAR(50) NULL DEFAULT NULL,
+  `Unitsofmeasurement` VARCHAR(255) NULL DEFAULT NULL,
   `Pricepurchase` DECIMAL(10,2) NULL DEFAULT NULL,
   `Pricerealization` DECIMAL(10,2) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  PRIMARY KEY (`id`),
+  INDEX `IDAssignationmedicament` (`IDAssignationmedicament` ASC) VISIBLE,
+  INDEX `IDProvider` (`IDProvider` ASC) VISIBLE,
+  CONSTRAINT `medicament_ibfk_1`
+    FOREIGN KEY (`IDAssignationmedicament`)
+    REFERENCES `pharmacy`.`assignationmedicament` (`id`),
+  CONSTRAINT `medicament_ibfk_2`
+    FOREIGN KEY (`IDProvider`)
+    REFERENCES `pharmacy`.`provider` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Таблица заказанных товаров
-CREATE TABLE IF NOT EXISTS `ordered` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `IDMedicament` INT NULL DEFAULT NULL,
-  `IDOrders` INT NULL DEFAULT NULL,
-  `Pricerealization` DECIMAL(10,2) NULL DEFAULT NULL,
-  `Quantity` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
 
--- Таблица заказов
-CREATE TABLE IF NOT EXISTS `orders` (
+-- -----------------------------------------------------
+-- Table `pharmacy`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `IDClient` INT NULL DEFAULT NULL,
   `IDEmployee` INT NULL DEFAULT NULL,
@@ -84,80 +124,44 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `Dateappointment` DATE NULL DEFAULT NULL,
   `Dateperformance` DATE NULL DEFAULT NULL,
   `Deliverycost` DECIMAL(10,2) NULL DEFAULT NULL,
-  `Recipient` VARCHAR(50) NULL DEFAULT NULL,
+  `Recipient` VARCHAR(255) NULL DEFAULT NULL,
   `Recipientaddress` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  PRIMARY KEY (`id`),
+  INDEX `IDClient` (`IDClient` ASC) VISIBLE,
+  INDEX `IDEmployee` (`IDEmployee` ASC) VISIBLE,
+  CONSTRAINT `orders_ibfk_1`
+    FOREIGN KEY (`IDClient`)
+    REFERENCES `pharmacy`.`client` (`id`),
+  CONSTRAINT `orders_ibfk_2`
+    FOREIGN KEY (`IDEmployee`)
+    REFERENCES `pharmacy`.`employee` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Таблица поставщиков
-CREATE TABLE IF NOT EXISTS `provider` (
+
+-- -----------------------------------------------------
+-- Table `pharmacy`.`ordered`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pharmacy`.`ordered` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(50) NULL DEFAULT NULL,
-  `Representative` VARCHAR(50) NULL DEFAULT NULL,
-  `Post` VARCHAR(50) NULL DEFAULT NULL,
-  `Phonenumber` VARCHAR(15) NOT NULL UNIQUE,
-  PRIMARY KEY (`id`)
-);
-
--- Таблица администраторов
-CREATE TABLE IF NOT EXISTS `administrator` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Fullname` VARCHAR(50) NOT NULL,
-  `Login` VARCHAR(50) NOT NULL UNIQUE,
-  `Password` VARCHAR(255) NOT NULL,
-  `Phonenumber` VARCHAR(15) NOT NULL UNIQUE,
-  PRIMARY KEY (`id`)
-);
-
--- Триггеры для хэширования паролей перед вставкой и обновлением в таблицах клиентов, сотрудников и администраторов
-DELIMITER //
-
-CREATE TRIGGER before_client_insert
-BEFORE INSERT ON client
-FOR EACH ROW
-BEGIN
-    CALL HashPassword(NEW.Password, @hashedPassword);
-    SET NEW.Password = @hashedPassword;
-END //
-
-CREATE TRIGGER before_client_update
-BEFORE UPDATE ON client
-FOR EACH ROW
-BEGIN
-    CALL HashPassword(NEW.Password, @hashedPassword);
-    SET NEW.Password = @hashedPassword;
-END //
-
-CREATE TRIGGER before_employee_insert
-BEFORE INSERT ON employee
-FOR EACH ROW
-BEGIN
-    CALL HashPassword(NEW.Password, @hashedPassword);
-    SET NEW.Password = @hashedPassword;
-END //
-
-CREATE TRIGGER before_employee_update
-BEFORE UPDATE ON employee
-FOR EACH ROW
-BEGIN
-    CALL HashPassword(NEW.Password, @hashedPassword);
-    SET NEW.Password = @hashedPassword;
-END //
-
-CREATE TRIGGER before_administrator_insert
-BEFORE INSERT ON administrator
-FOR EACH ROW
-BEGIN
-    CALL HashPassword(NEW.Password, @hashedPassword);
-    SET NEW.Password = @hashedPassword;
-END //
-
-CREATE TRIGGER before_administrator_update
-BEFORE UPDATE ON administrator
-FOR EACH ROW
-BEGIN
-    CALL HashPassword(NEW.Password, @hashedPassword);
-    SET NEW.Password = @hashedPassword;
-END //
-
-DELIMITER ;
+  `IDMedicament` INT NULL DEFAULT NULL,
+  `Pricerealization` DECIMAL(10,2) NULL DEFAULT NULL,
+  `Quantity` INT NULL DEFAULT NULL,
+  `orders_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `IDMedicament` (`IDMedicament` ASC) VISIBLE,
+  INDEX `fk_ordered_orders1_idx` (`orders_id` ASC) VISIBLE,
+  CONSTRAINT `ordered_ibfk_1`
+    FOREIGN KEY (`IDMedicament`)
+    REFERENCES `pharmacy`.`medicament` (`id`),
+  CONSTRAINT `fk_ordered_orders1`
+    FOREIGN KEY (`orders_id`)
+    REFERENCES `pharmacy`.`orders` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
